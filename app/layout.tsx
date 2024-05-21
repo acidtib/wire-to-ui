1422c6336f7f7c80592a3efc760dee53efdcf3f5
acidtib <arubinofaux@gmail.com>
2024-05-21 16:10:48 -0600

    update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-	title: 'make real starter',
-	description: 'draw a website and make it real',
+	title: 'wire-to-ui ::  Generate Tailwind HTML/CSS based on a wireframe',
+	description: 'This is an app that uses tldraw and the gpt-4-vision api to generate tailwind html/css based on a wireframe you draw.',
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
